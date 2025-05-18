1209b3e07fe53174dc17f23cc38d896b654f99a0
Manish Kumar <manish98uuu@gmail.com>
2025-05-18 22:59:52 +0530

    Pass by value

---
+++ prog37.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void change(int);
+int main(){
+    int n=10;
+    cout <<"Before changing n's value ="<<n<<endl;
+    change(n);
+    cout<<"After changing n's value="<<n<<endl;
+    return 0;
+}
+void change(int p){
+    p++;
+}
